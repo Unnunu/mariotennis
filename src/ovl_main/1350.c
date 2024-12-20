274ea16678c98f184f639c4c510158a3fd449eca
Unnunu <serge_zorkin@mail.ru>
2024-12-20 16:28:27 +0300

    saving local progress

--- src/ovl_main/1350.c
+++ src/ovl_main/1350.c
@@ -23,3 +23,44 @@ INCLUDE_ASM("asm/nonmatchings/ovl_main/1350", func_80031AAC);
 INCLUDE_ASM("asm/nonmatchings/ovl_main/1350", func_80031B50);
 
 INCLUDE_ASM("asm/nonmatchings/ovl_main/1350", func_80031C4C);
+/*
+#include "PR/os_internal.h"
+#include "PR/ultraerror.h"
+#include "PRinternal/osint.h"
+
+void func_80031C4C(OSThread* t) {
+    register u32 saveMask = __osDisableInt();
+
+    switch (t->state) {
+        case OS_STATE_WAITING:
+            t->state = OS_STATE_RUNNABLE;
+            __osEnqueueThread(&__osRunQueue, t);
+            break;
+        case OS_STATE_STOPPED:
+            if (t->queue == NULL || t->queue == &__osRunQueue) {
+                t->state = OS_STATE_RUNNABLE;
+                __osEnqueueThread(&__osRunQueue, t);
+            } else {
+                t->state = OS_STATE_WAITING;
+                __osEnqueueThread(t->queue, t);
+                __osEnqueueThread(&__osRunQueue, __osPopThread(t->queue));
+            }
+            break;
+#ifdef _DEBUG
+        default:
+            __osError(ERR_OSSTARTTHREAD, 0);
+            __osRestoreInt(saveMask);
+            return;
+#endif
+    }
+
+    if (__osRunningThread == NULL) {
+        __osDispatchThread();
+    } else if (__osRunningThread->priority < __osRunQueue->priority) {
+        __osRunningThread->state = OS_STATE_RUNNABLE;
+        __osEnqueueAndYield(&__osRunQueue);
+    }
+
+    __osRestoreInt(saveMask);
+}
+*/
---
+++ src/ovl_main/20B0.c
@@ -0,0 +1,34 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031D60);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031DC4);
+
+void func_80031DD0(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031DD8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031E04);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031E94);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031EB0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031ECC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031EE8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031F0C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80031F40);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_800321C4);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80032200);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_8003223C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_80032278);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_main/20B0", func_800322B4);
